6d3e23d773c00a71654135ac437b37f1bc7f35f6
Aleksandr214 <aleksandrkocurov2144@gmail.com>
2023-10-20 20:32:07 +0400

    сохронил

--- Program.cs
+++ Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿int [] GenerateArray(int sizeArray, int leftRange, int rightRange)
+{
+    int[]newArray=new int[sizeArray];
+    Random rand=new Random();
+    for (int i = 0; i < newArray.Length; i++)
+    {
+       newArray[i]=rand.Next(leftRange,rightRange+1); 
+    }
+    return newArray;
+}
+void PrintArray(int[]array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]+ " ");
+    }
+}
+void Array(int[]array)
+{
+int Count = 0;
+for (int i = 0; i < array.Length; i++)
+if (array[i] % 2 == 0)
+Count++;
+Console.WriteLine($"всего {array.Length} чисел, {Count} из них чётные");
+}
+
+int[] array=GenerateArray(7,100,999);
+PrintArray(array);
+
+Array(array);
+//PrintArray(array);
